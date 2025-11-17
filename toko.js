0888749ab8d30a3e49c7d5440bb29fe3034a5800
nahrulirham <irhamnahrul344@gmail.com>
2025-11-17 19:31:13 +0700

    Create toko.js

---
+++ toko.js
@@ -0,0 +1,18 @@
+var x = 1500;
+console.log("nilai dari variable x adalah " + x);
+
+//pop up box
+//1. allert
+alert("tolong di ok kontol ");
+
+//2. prompt
+var nama = prompt("Masukkan nama anda");
+alert("halo " + nama);
+
+//3.confirm
+var konfirmas = confirm("apakah anda yakin lanjut?");
+if (konfirmasi === true) {
+  alert("pekerjaan sedang berlanjut");
+} else {
+  alert("pekerjaan selesai");
+}
